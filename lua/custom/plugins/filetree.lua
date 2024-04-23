d8db26297ea81d882f3d37ceac67abaad1a2ca3d
mmiddlezong <119022671+mmiddlezong@users.noreply.github.com>
2024-04-22 21:27:12 -0400

    LaTeX fixes

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+    vim.keymap.set('n', '<leader>E', '<Cmd>Neotree toggle current reveal_force_cwd<CR>', { desc = 'Toggle file [E]xplorer' })
+  end,
+}
